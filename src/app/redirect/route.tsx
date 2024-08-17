91bca10c8f300de0619122ea03f45d74d1da54ae
jennaleonard <jleonardweb@gmail.com>
2024-08-16 18:11:45 -0700

    followed set up of pinata tutorial

--- src/app/redirect/route.tsx
+++ src/app/redirect/route.tsx
@@ -1,49 +1,10 @@
-/* eslint-disable react/jsx-key */
-import { Button, button } from "frames.js/core";
-import { getTokenUrl } from "frames.js";
-import { frames } from "../frames/frames";
+import { NextRequest, NextResponse } from "next/server";
 
-function generateRandomIndex(): number {
-  const minIndex = 0;
-  const maxIndex = 155;
-
-  return Math.floor(Math.random() * (maxIndex - minIndex + 1)) + minIndex;
+export async function POST(req: NextRequest, res: NextResponse){
+  try {
+   return NextResponse.redirect('https://pinata.cloud', { status: 302 }) 
+  } catch (error) {
+   console.log(error) 
+    return NextResponse.json({ error: error })
+  }
 }
-
-const randomIndex = generateRandomIndex();
-
-const handleRequest = frames(async (ctx) => {
-  return {
-    image: (
-      <span>
-        {ctx.pressedButton
-          ? `I clicked ${ctx.searchParams.value}`
-          : `Click some button`}
-      </span>
-    ),
-    buttons: [
-      <Button
-        action="mint"
-        target={getTokenUrl({
-          address: "0x67D7e7BCc964De5BEf0951EB818E3A9A136312B5",
-          chain: zora,
-          tokenId: BigInt(randomIndex + 1).toString(),
-        })}
-      >
-        Mint
-      </Button>,
-      button({
-        action: "mint",
-        target: getTokenUrl({
-          address: "0x67D7e7BCc964De5BEf0951EB818E3A9A136312B5",
-          chain: zora,
-          tokenId: BigInt(randomIndex + 1).toString(),
-        }),
-        label: "Mint",
-      }),
-    ],
-  };
-});
-
-export const GET = handleRequest;
-export const POST = handleRequest;
